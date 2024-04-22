db4466e95e635fe04f1afadc52357b347ac0cd30
Sabri Taieb <sabritaiebdev@gmail.com>
2024-04-22 10:56:10 +0100

    Add password-reset bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
