d84438d264aa0456707f114242c972e7e7ce2a9e
sabri <sabritaiebdev@gmail.com>
2024-04-30 09:35:39 +0100

    Add Admin controller and index template

--- app/src/Controller/Admin/AdminController.php
+++ app/src/Controller/Admin/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +12,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin_index')]
-    public function index(): Response
+    public function index(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository,
+        UserRepository $userRepository
+    ): Response
     {
         return $this->render('admin/index.html.twig', [
-            'productsCount' => 0,
-            'categoriesCount' => 0,
-            'usersCount' => 0,
+            'productsCount' => $productRepository->count(),
+            'categoriesCount' => $categoryRepository->count(),
+            'usersCount' => $userRepository->count(),
         ]);
     }
 }
