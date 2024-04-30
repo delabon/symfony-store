6cbd7cd832735777ed1552d050c93baffd3536d0
sabri <sabritaiebdev@gmail.com>
2024-04-30 10:09:17 +0100

    Make sure only admin can access admin pages

--- app/src/Controller/Admin/AdminController.php
+++ app/src/Controller/Admin/AdminController.php
@@ -8,7 +8,9 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin_index')]
--- app/src/Controller/Admin/CategoryController.php
+++ app/src/Controller/Admin/CategoryController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/categories', name: 'admin_category_')]
+#[IsGranted('ROLE_ADMIN')]
 class CategoryController extends AbstractController
 {
     #[Route('', name: 'index')]
