4b1603fbd5dae0acc8e56e5b642833d517ecd8b4
sabri <sabritaiebdev@gmail.com>
2024-04-30 08:33:45 +0100

    Add admin controller and twig template

---
+++ app/src/Controller/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin_index')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'productsCount' => 0,
+            'categoriesCount' => 0,
+            'usersCount' => 0,
+        ]);
+    }
+}
