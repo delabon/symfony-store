769f027e88fc76d0b80d0c8fbedb5b4b8fc8214a
sabri <sabritaiebdev@gmail.com>
2024-05-01 05:51:40 +0100

    Update header tempplate

--- app/src/Controller/Admin/AdminController.php
+++ app/src/Controller/Admin/AdminController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin_index')]
+    #[Route('/admin', name: 'admin_index')]
     public function index(
         ProductRepository $productRepository,
         CategoryRepository $categoryRepository,
