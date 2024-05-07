4d08f2c9a498b5ff7e4004be41cdd31b1f9820e2
sabri taieb <sabritaiebdev@gmail.com>
2024-05-07 09:54:56 +0100

    Add contact page and its functionality

---
+++ app/src/Controller/ContactController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\DTO\ContactDTO;
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact_index')]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class, new ContactDTO());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactDTO = $form->getData();
+
+            $email = (new TemplatedEmail())
+                ->from($contactDTO->getEmail())
+                ->to(new Address($this->getParameter('app_support_email')))
+                ->subject('New contact message from ' . $contactDTO->getName())
+                ->htmlTemplate('contact/email.html.twig')
+                ->context([
+                    'name' => $contactDTO->getName(),
+                    'emailAddress' => $contactDTO->getEmail(),
+                    'message' => $contactDTO->getMessage(),
+                ]);
+
+            try {
+                $mailer->send($email);
+                $this->addFlash('success', 'Your message has been sent.');
+            } catch (TransportExceptionInterface $e) {
+                $this->addFlash('error', 'An error occurred while sending your message.');
+            }
+
+            return $this->redirectToRoute('app_contact_index');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ app/src/Controller/DTO/ContactDTO.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactDTO
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 2, max: 255)]
+        #[Assert\Regex(pattern: '/^[a-z\s]+$/i', message: 'Name can only contain letters and spaces')]
+        private string $name = '',
+        #[Assert\NotBlank]
+        #[Assert\Email]
+        private string $email = '',
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 10, max: 255)]
+        private string $message = ''
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+}
