eb7f94bcea57a35c8b566cdc43e6f046e557954c
sabri taieb <sabritaiebdev@gmail.com>
2024-05-07 08:16:00 +0100

    Add Page fixtures

---
+++ app/src/DataFixtures/PageFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Page;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PageFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 25; $i++) {
+            $page = new Page();
+            $page->setName('Page ' . $i);
+            $page->setSlug('page-' . $i);
+            $page->setContent(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus ut nunc vestibulum ultricies. Nullam nec purus ut nunc vestibulum ultricies. Nullam nec purus ut nunc vestibulum ultricies. Nullam nec purus ut nunc vestibulum ultricies.'
+            );
+            $page->setCreatedAt(new \DateTimeImmutable());
+            $page->setUpdatedAt(new \DateTimeImmutable());
+            $page->setUser($this->getReference('user_' . $i));
+            $manager->persist($page);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+}
