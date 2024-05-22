da88f42b9b012c30e29c948f411a0c7db29da0ca
sabri taieb <sabritaiebdev@gmail.com>
2024-05-22 07:56:33 +0100

    Add order controller and orders basic template

---
+++ app/src/Controller/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+#[Route('/orders', name: 'app_order_')]
+class OrderController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('order/index.html.twig', [
+            'controller_name' => 'OrderController',
+        ]);
+    }
+}
