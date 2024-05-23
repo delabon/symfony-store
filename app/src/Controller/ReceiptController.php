70968604c5f56968d90b8fad529ef9b2c9660df5
sabri taieb <sabritaiebdev@gmail.com>
2024-05-23 15:52:24 +0100

    Add receipt page

--- app/src/Controller/ReceiptController.php
+++ app/src/Controller/ReceiptController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class ReceiptController extends AbstractController
 {
     #[Route('/receipt/{uid}', name: 'app_receipt_show')]
-    public function show(string $uid): Response
+    public function show(
+        string $uid,
+        OrderRepository $orderRepository
+    ): Response
     {
-        dd($uid);
+        $order = $orderRepository->findOneBy(['uniqueId' => $uid]);
 
-        return $this->render('receipt/index.html.twig', [
-            'controller_name' => 'ReceiptController',
+        if (!$order) {
+            return new Response('No receipt found', Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->render('receipt/show.html.twig', [
+            'order' => $order,
         ]);
     }
 }
