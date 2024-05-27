9b52cd256d1d108d044264ed436ed758aeb0f720
sabri taieb <sabritaiebdev@gmail.com>
2024-05-27 18:42:10 +0100

    Add product single page

---
+++ app/src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Service\ThumbnailService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product/{slug}', name: 'app_product_show')]
+    public function show(Product $product, ThumbnailService $thumbnailService): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+            'thumbnail' => $product->getThumbnailId() ? $thumbnailService->getUrl($product->getThumbnailId(), 500, 500) : null,
+        ]);
+    }
+}
--- app/src/DataFixtures/ProductFixtures.php
+++ app/src/DataFixtures/ProductFixtures.php
@@ -4,23 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Product;
 use App\Enum\EntityStatusEnum;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class ProductFixtures extends Fixture implements DependentFixtureInterface
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 30; $i++) {
             $product = new Product();
-            $product->setName("Product {$i}");
-            $product->setSlug("product-{$i}");
-            $product->setDescription("Description for product {$i}");
+            $product->setName($this->faker->name());
+            $product->setSlug($this->faker->slug());
+            $product->setDescription(implode("\r\n\r\n", $this->faker->paragraphs(4)));
             $product->setPrice(mt_rand(1000, 10000) / 100);
             $product->setSalePrice($product->getPrice() - 0.1);
-            $product->setCreatedAt(new \DateTimeImmutable());
-            $product->setUpdatedAt(new \DateTimeImmutable());
+            $date = DateTimeImmutable::createFromMutable($this->faker->dateTime());
+            $product->setCreatedAt($date);
+            $product->setUpdatedAt($date);
             $product->setUser($this->getReference('admin'));
             $product->setCategory($this->getReference('category_' . mt_rand(0, 4)));
             $product->setStatus(EntityStatusEnum::toArray()[array_rand(EntityStatusEnum::toArray())]);
